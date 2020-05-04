b35de01db2373d99f084804e2b37998c2ac04842
prade091078 <prade.accet@gmail.com>
2020-05-05 03:20:56 +0530

    Added ad Mob

--- app/src/main/java/com/prade/firebasedemo/MainActivity.java
+++ app/src/main/java/com/prade/firebasedemo/MainActivity.java
@@ -5,6 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -19,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
+      /*  DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
         Map<String, String> values =new HashMap<>();
         values.put("name","John");
         dbRef.push().setValue(values, new DatabaseReference.CompletionListener() {
@@ -31,6 +36,18 @@ public class MainActivity extends AppCompatActivity {
                     Log.i("Info","Save failed");
                 }
             }
+        });*/
+
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(InitializationStatus initializationStatus) {
+            }
         });
+
+        AdView adView = (AdView)findViewById(R.id.adView);
+        AdRequest request=new AdRequest.Builder().build();
+        adView.loadAd(request);
+
+
     }
 }
