4b95fe30ff68e081ceb6aef3d05865ba730572d5
prade091078 <prade.accet@gmail.com>
2020-05-05 02:31:47 +0530

    Firebase Demo

---
+++ app/src/main/java/com/prade/firebasedemo/MainActivity.java
@@ -0,0 +1,36 @@
+package com.prade.firebasedemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
+        Map<String, String> values =new HashMap<>();
+        values.put("name","John");
+        dbRef.push().setValue(values, new DatabaseReference.CompletionListener() {
+            @Override
+            public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+                if (databaseError == null) {
+                    Log.i("info","Save successful");
+                } else {
+                    Log.i("Info","Save failed");
+                }
+            }
+        });
+    }
+}
